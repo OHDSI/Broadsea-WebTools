10245405d7a1f455153172d7f1e72c6a72d613d3
chgl <chgl@users.noreply.github.com>
2020-03-27 23:27:35 +0100

    added ability to set webapi url in config-local.js via env var

--- deploy-script.sh
+++ deploy-script.sh
@@ -1,5 +1,14 @@
-# update the Penelope configuration URL of the WebAPI
-perl -i -pe "BEGIN{undef $/;} s|http://localhost:8080|"$WEBAPI_URL"|g" /usr/local/tomcat/webapps/penelope/web/js/app.js
+#!/bin/bash
+
+# make sure the WebAPI URL ends with a slash
+if [[ "$WEBAPI_URL" != */ ]]; then
+    WEBAPI_URL="$WEBAPI_URL/"
+fi
+
+# if set, replace the webapi URL in the config file with the one in the WEBAPI_URL env var
+if [ -v WEBAPI_URL ]; then
+    sed -i "s,http://localhost:8080/WebAPI/,$WEBAPI_URL,g" /usr/local/tomcat/webapps/atlas/js/config-local.js
+fi
 
 # load any jdbc drivers in the docker host volume mapped directory into the tomcat library
 cp /tmp/drivers/*.jar /usr/local/tomcat/lib
