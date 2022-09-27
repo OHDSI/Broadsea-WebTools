ae48adae7792372e91af5999f44fc77693020f2d
Lee Evans <leeevans@users.noreply.github.com>
2022-09-27 12:37:34 -0400

    remove Penelope config code

--- deploy-script.sh
+++ deploy-script.sh
@@ -1,6 +1,3 @@
-# update the Penelope configuration URL of the WebAPI
-perl -i -pe "BEGIN{undef $/;} s|http://localhost:8080|"$WEBAPI_URL"|g" /usr/local/tomcat/webapps/penelope/web/js/app.js
-
 # load any jdbc drivers in the docker host volume mapped directory into the tomcat library
 cp /tmp/drivers/*.jar /usr/local/tomcat/lib
 
